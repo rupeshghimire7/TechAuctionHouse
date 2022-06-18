d9a116bd5457b780b7dbb3976cfdd331911312a3
Sandesh GC <gcsandesh01@gmail.com>
2022-06-18 18:43:36 +0545

    added styles

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/',views.login, name='login'),
-    path('items',views.items, name='items'),
-    path('about',views.about, name='about')
+    path('login',views.login, name='login'),
+    path('items/<title>',views.items, name='items'),
+    path('about',views.about, name='about'),
+    path('contact',views.contact, name='contact')
 ]
