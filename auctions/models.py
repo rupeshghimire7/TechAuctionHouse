abcc77d02455891acf6b0d86831e493b8c3f91e4
Rupesh Ghimire <rupeshghimire007@gmail.com>
2022-06-19 23:35:23 +0545

    Created model and superuser

--- auctions/models.py
+++ auctions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    price = models.FloatField()
+    description = models.CharField(max_length=1000)
+    publish_date = models.DateField()
+    bid_ends = models.DateField()
+    image = models.ImageField(upload_to='auctions/product_images', default='')
