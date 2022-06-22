4d3cc2384d3c6e19f9e407ab0b682a9efc19285e
saileshghimire <saileshghimire1@gmail.com>
2022-06-22 06:57:09 +0545

    signup basic

--- TechAuctionHouse/views.py
+++ TechAuctionHouse/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+#create your views here:
+def sign_up(request):
+    fm=UserCreationForm()
+    return render(request,'auctions/signup.html',{'form':fm})
+
+
